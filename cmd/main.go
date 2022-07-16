7da9887e9ab03e3f7bdc69ae60dc9c99d50e85e0
Gleb Kalinin <sogener.g@gmail.com>
2022-07-16 13:54:42 +0500

    Implement postgres driver sqlx

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 	"toDo"
@@ -14,7 +15,20 @@ func main() {
 		log.Fatalf("error while inititalization config %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("DATABASE_HOST"),
+		Port:     viper.GetString("DATABASE_PORT"),
+		Username: viper.GetString("DATABASE_USERNAME"),
+		Password: viper.GetString("DATABASE_PASSWORD"),
+		DBName:   viper.GetString("DATABASE_NAME"),
+		SSLMode:  viper.GetString("DATABASE_SSL_MODE"),
+	})
+
+	if err != nil {
+		log.Fatalf("error while connecting to the database %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
